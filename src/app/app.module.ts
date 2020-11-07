e077f60677af51848b60f5150f7e0ec75363f520
HendrikCromboom <hcromboom@gmail.com>
2020-11-07 21:33:05 +0100

    components init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProperyCardCompoment } from './property/property-card/property-card.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+    ProperyCardCompoment,
+    PropertyListComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule
   ],
