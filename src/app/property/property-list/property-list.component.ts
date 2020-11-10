5320efa72e8729d8881269b95afb65dee8e09081
HendrikCromboom <hcromboom@gmail.com>
2020-11-10 10:59:02 +0100

    httpservices done

--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HousingService } from 'src/app/services/housing.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -11,15 +11,16 @@ export class PropertyListComponent implements OnInit {
 
   properties: any; // properties json was hardcoded here before
 
-  constructor(private http:HttpClient) { }
+  constructor(private housingService: HousingService) { }
 
   ngOnInit(): void {
-    this.http.get('data/properties.json').subscribe(
-      data=>{
-        this.properties = data;
-      data=>console.log(data);
-      }
-    );
+   this.housingService.getAllProperties().subscribe(
+     data=>{
+       this.properties=data;
+       console.log(data);
+     }, error=>
+     console.log(error)
+   )
   }
 
 }
--- src/app/services/housing.service.ts
+++ src/app/services/housing.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HousingService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getAllProperties(){
+    return this.http.get('data/properties.json')
+  }
 }
